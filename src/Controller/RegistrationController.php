33a3f5377d1b4b998d7999c49ad6d710c73bc852
E.Bousk <e.bousk@gmail.com>
2022-02-03 13:36:39 +0100

    Make registration controller - Install bootstrap

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(
+        Request $request,
+        UserPasswordEncoderInterface $userPasswordEncoder, 
+        EntityManagerInterface $entityManager,
+        TokenGeneratorInterface $tokenGenerator
+    ): Response
+    {
+        $user = new User();
+        
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRegistrationToken($tokenGenerator->generateToken())
+                ->setPassword($userPasswordEncoder->encodePassword($user, $form->get('password')->getData()))
+            ;
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre compte utilisateur a bien été créé.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
